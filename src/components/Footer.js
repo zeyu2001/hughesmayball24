e2ca27bc9f458c1f777a27783cf21e33ad4d2947
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-01-19 16:47:16 +0000

    fix: mobile navbar transparent

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer class="bg-white rounded-lg shadow">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-          <span class="self-center whitespace-nowrap">Hughes Hall May Ball 2024</span>
+  <footer className="bg-white rounded-lg shadow">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <span className="self-center whitespace-nowrap">Hughes Hall May Ball 2024</span>
         </a>
-        <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+        <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
           <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Tickets</a>
+            <a href="#" className="hover:underline me-4 md:me-6">Tickets</a>
           </li>
           <li>
-            <a href="#" class="hover:underline">Contact</a>
+            <a href="#" className="hover:underline">Contact</a>
           </li>
         </ul>
       </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-      <span class="block text-sm text-gray-500 sm:text-center">© 2024 <a href="https://flowbite.com/" class="hover:underline">Hughes Hall May Ball 2024 Committee</a>. All Rights Reserved.</span>
+      <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+      <span className="block text-sm text-gray-500 sm:text-center">© 2024 <a href="https://flowbite.com/" className="hover:underline">Hughes Hall May Ball 2024 Committee</a>. All Rights Reserved.</span>
     </div>
   </footer>
 );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -46,7 +46,7 @@ export default function Nav() {
     </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between text-white">
             <a href="#" className="-m-1.5 p-1.5">
               <span>The World's Fair at Hughes Hall</span>
