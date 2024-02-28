eecf7b24c62f195b51dc983201b7eb8c5991199f
Zhang Zeyu <zhangzeyu2001@gmail.com>
2024-02-28 00:44:50 +0000

    fix: instagram widget on mobile

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -2,10 +2,8 @@ const Instagram = () => {
   return (
     <section className="bg-gray-900 flex items-center justify-center h-screen" id="instagram">
       <iframe src="https://www.instagram.com/hughesmayball/embed" 
-        className="rounded-lg"
+        className="rounded-lg h-full sm:w-full md:w-3/4 lg:w-1/2"
         title="Instagram"
-        width="50%"
-        height="100%"
       >
       </iframe>
     </section>
